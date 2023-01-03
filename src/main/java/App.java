a611e25c2da2b650b116ba05d509295f26515491
olgak <olga.kim0602@gmail.com>
2023-01-03 20:02:34 +0900

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean1 == bean2);
+
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean3==bean4);
+
+
+
+    }
+
+}
